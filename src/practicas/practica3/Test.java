c4061c5821501b579f7172ffeff048798f8a7ed4
David Perez Gomez <davidperezgo@gmail.com>
2018-02-22 18:39:34 +0100

    añadida clase para testeo manual desde codigo

---
+++ src/practicas/practica3/Test.java
@@ -0,0 +1,12 @@
+package practicas.practica3;
+
+public class Test {
+
+        public static void main(String[] args) {
+        KNNParameterEval parameterEval = new KNNParameterEval();
+//        parameterEval.evaluateKParameter("./src/files/balance-scale.arff", 4, false, true);
+//        parameterEval.evaluateKParameter("./src/files/wine.arff", 0, true, true);
+//        parameterEval.evaluateAllParameters("./src/files/balance-scale.arff", 4, false, true);
+        parameterEval.evaluateAllParameters("./src/files/wine.arff", 0, true, true);
+    }
+}
